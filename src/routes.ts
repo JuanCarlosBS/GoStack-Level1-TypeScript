7a735e86ce43d05a39514a1355ed8b7b179249ff
juancarlosBS <juanrov_@hotmaail.com>
2020-07-22 03:06:06 -0300

    typing and array in ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,18 @@
-import { Request, Response } from 'express'
+import { Request, Response, text } from 'express'
+import createUser from './services/CreateUser'
 
 export function helloWorld(req: Request, res: Response) {
+    const user = createUser({ 
+        name: 'juan',
+        email: 'juanrov_@hotmail.com',
+        password: '123',
+        tech: [
+            'nodejs',
+            'reactjs',
+            'react native',
+            { title: 'JavaScript', experience: 100}
+        ]
+    });
+
     return res.json({ message: 'Hello World'})
 }
